57589053a6e2a7d3130bbcfe932910a77505b50d
Chen Ben Ami <chen24201@gmail.com>
2023-01-15 21:16:06 +0200

    final

--- algo.c
+++ algo.c
@@ -49,7 +49,6 @@ void build_graph_cmd(char *userInput, int end) {
     if (graph != NULL) {
         deleteGraph_cmd();
     }
-//    printf("%s\n", userInput);
     graph = malloc(sizeof(struct Graph_));
     graph->amountOfNodes = 0;
     graph->amountOfEdges = 0;
@@ -66,25 +65,18 @@ void build_graph_cmd(char *userInput, int end) {
         int j = countBetween(userInput, pointer);
         for (int k = 0; k < j; k++) {
             if (counter == 0) {
-//                printf("%c asdasd\n", userInput[pointer]);
                 int index = getNum(userInput, pointer, num);
                 sscanf(num, "%d", &number);
-//                printf("num is %d\n", number);
                 src = createNode(number);
                 number = 0;
                 k += index;
-//                printf("src index %d pointer is %d and then pointer is %d\n", index, pointer, pointer + index);
                 pointer += index;
-//                printf("%c", userInput[pointer]);
-
             } else if (counter % 2 == 1) {
                 int index = getNum(userInput, pointer, num);
                 sscanf(num, "%d", &number);
                 dest = createNode(number);
-//                printf("dest num is %d\n", number);
                 number = 0;
                 k += index;
-//                printf("dest index %d pointer is %d and then pointer is %d\n", index, pointer, pointer + index);
                 pointer += index;
 
 
@@ -92,17 +84,10 @@ void build_graph_cmd(char *userInput, int end) {
                 int index = getNum(userInput, pointer, num);
                 sscanf(num, "%d", &number);
                 k += index;
-//                printf("edge index %d pointer is %d and then pointer is %d\n", index, pointer, pointer + index);
                 pointer += index;
-//                printf("**************\n");
-//                printf("src-> data %d\n", src->data);
-//                printf("dest-> data %d\n", dest->data);
-//                printf("weight = %d\n", number);
-//                printf("**************\n");
                 addEdge(src, dest, number);
                 number = 0;
             }
-            //A 4 n 0 2 5 3 3 n 2 0 4 1 1 n 1 3 7 0 2 n 3 T 3 2 1 3 S 2 0
             pointer++;
             if (userInput[pointer] == 'B' || userInput[pointer] == 'D' || userInput[pointer] == 'T' || userInput[pointer] == 'S' ||
                 userInput[pointer] == 'A') {
@@ -120,11 +105,7 @@ void build_graph_cmd(char *userInput, int end) {
 
         if (userInput[pointer] == ' ') pointer++;
     }
-//    printf("\n");
     free(num);
-//    printf("******************\n");
-//    printGraph_cmd();
-//    printf("******************\n");
 }
 
 void insert_node_cmd(char *userInput, int end) {
@@ -181,7 +162,7 @@ int nextLetter(char *userInput) {
     return -1;
 }
 
-
+//debug
 void printGraph_cmd() {
     pedge edge = NULL;
     pnode node = graph->first_node;
@@ -219,17 +200,13 @@ pnode createNode(int data) {
     pnode node = NULL;
     node = findNode(data);
     if (node != NULL) {
-        //node exist
         return node;
     }
-    //node doesn't exist
-    // Allocate memory for the new Node
     node = (pnode) malloc(sizeof(Node));
     node->data = data;
     node->first_edge = NULL;
     node->last_edge = NULL;
     node->next = NULL;
-    // Add the Node to the array of first_node
     if (graph->last_node == NULL) {
         graph->last_node = node;
         graph->first_node = node;
@@ -237,7 +214,6 @@ pnode createNode(int data) {
         graph->last_node->next = node;
         graph->last_node = node;
     }
-    // Increment the number of first_node in the Graph
     graph->amountOfNodes++;
     return node;
 }
@@ -271,14 +247,12 @@ pnode findNode(int data) {
         }
         node = node->next;
     }
-    // No node with the given name was found
     return NULL;
 }
 
 
 void deleteOutwardEdges(int data) {
     pnode node = findNode(data);
-    // Free the memory for the first_edge
     pedge edge = node->first_edge;
     while (edge != NULL) {
         pedge temp = edge;
@@ -286,23 +260,19 @@ void deleteOutwardEdges(int data) {
         graph->amountOfEdges--;
         free(temp);
     }
-    // Reset the first_edge field for the node
     node->first_edge = NULL;
     node->last_edge = NULL;
 }
 
 void deleteInComingEdges(int data) {
     pnode node = graph->first_node;
-    // Iterate through the first_node and first_edge of the graph
     while (node != NULL) {
         pedge edge = node->first_edge;
         pedge prev = NULL;
         while (edge != NULL) {
             if (edge->endpoint->data == data) {
-                // This edge has the given endpoint node as the destination
                 pedge temp = edge;
                 if (prev == NULL) {
-                    // This is the first edge in the linked list
                     node->first_edge = edge->next;
                 } else {
                     prev->next = edge->next;
@@ -320,9 +290,7 @@ void deleteInComingEdges(int data) {
 }
 
 int shortsPath_cmd(int src, int dest) {
-//    printf("start is %d end is %d\n", src, dest);
     pnode pointer = graph->first_node;
-    // initialize all distances to infinity
     while (pointer != NULL) {
         if (pointer->data == src) {
             pointer->distance = 0;
@@ -350,21 +318,15 @@ int shortsPath_cmd(int src, int dest) {
 
 void permute(int *arr, int start, int end, int **result, int *count) {
     if (start == end) {
-        // save the current permutation
         for (int i = 0; i <= end; i++)
             result[*count][i] = arr[i];
         (*count)++;
     } else {
         for (int i = start; i <= end; i++) {
-            // swap the current element with the start
             int temp = arr[start];
             arr[start] = arr[i];
             arr[i] = temp;
-
-            // recursively generate permutations for the remaining elements
             permute(arr, start + 1, end, result, count);
-
-            // swap the current element back to its original position
             temp = arr[start];
             arr[start] = arr[i];
             arr[i] = temp;
@@ -390,11 +352,6 @@ int factorial(int n) {
 }
 
 void TSP_cmd(int nodes[], int k) {
-//    for (int i = 0; i < k; ++i) {
-//        printf("%d,", nodes[i]);
-//    }
-//    printf("\n");
-
     int **distArr = (int **) calloc((k + 1), sizeof(int *));
     for (int i = 0; i < k + 1; i++) {
         distArr[i] = (int *) calloc((k + 1), sizeof(int));
@@ -420,35 +377,15 @@ void TSP_cmd(int nodes[], int k) {
         result[i] = (int *) malloc(sizeof(int) * k);
     }
     permute(nodes, 0, k - 1, result, &count);
-//    for (int i = 0; i < factRes; ++i) {
-//        for (int j = 0; j < k; ++j) {
-//            printf("%d,", result[i][j]);
-//        }
-//        printf("\n");
-//    }
     bool isPath = false;
     int cur_path_length;
     int min_path_length = INT_MAX;
     int i = 0;
-//    printf("******************\n");
-//    printGraph_cmd();
-//    printf("******************\n");
-//    for (int i = 0; i < k + 1; ++i) {
-//        for (int j = 0; j < k + 1; ++j) {
-//            printf("%d,", distArr[i][j]);
-//        }
-//        printf("\n");
-//    }
-//    printf("******************\n");
     while (i < factRes) {
         cur_path_length = 0;
         for (int j = 0; j < k - 1; ++j) {
-//            printf("we were on the lookout for many criminals %d and big tity gf %d\n", result[i][j], result[i][j + 1]);
             int index1 = find(distArr, result[i][j], k);
             int index2 = find(distArr, result[i][j + 1], k);
-//            printf("index1 %d\n", index1);
-//            printf("index2 %d\n", index2);
-//            printf("distArr[index1][index2] %d\n", distArr[index1][index2]);
             cur_path_length += distArr[index1][index2];
             if (cur_path_length >= INT_MAX / 2 || cur_path_length <= 0) {
                 break;
@@ -482,25 +419,19 @@ void TSP_cmd(int nodes[], int k) {
 void deleteEmptyNode(int data) {
     pnode temp = graph->first_node;
     pnode prev;
-    // If head node itself holds the key to be deleted
     if (temp != NULL && temp->data == data) {
-        temp = temp->next; // Changed head
-        free(temp); // free old head
+        temp = temp->next;
+        free(temp);
         return;
     }
-    // Search for the key to be deleted, keep track of the
-    // previous node as we need to change 'prev->next'
     while (temp != NULL && temp->data != data) {
         prev = temp;
         temp = temp->next;
     }
-    // If key was not present in linked list
     if (temp == NULL)
         return;
-    // Unlink the node from linked list
     prev->next = temp->next;
     free(temp); // Free memory
-    // Decrement the number of first_node in the graph
     graph->amountOfNodes--;
 }
 
@@ -515,7 +446,6 @@ void delete_node_cmd(int data) {
 
 void deleteGraph_cmd() {
     if (graph != NULL) {
-        // Free the memory for the first_edge
         pnode node = graph->first_node;
         while (node != NULL) {
             pedge edge = node->first_edge;
@@ -526,14 +456,12 @@ void deleteGraph_cmd() {
             }
             node = node->next;
         }
-        // Free the memory for the first_node
         node = graph->first_node;
         while (node != NULL) {
             pnode temp = node;
             node = node->next;
             free(temp);
         }
-        // Reset the fields of the graph struct
         graph->first_node = NULL;
         graph->last_node = NULL;
         graph->amountOfNodes = 0;
@@ -542,20 +470,3 @@ void deleteGraph_cmd() {
     }
     graph = NULL;
 }
-
-void deleteGraph() {
-    pnode currentNode = graph->first_node;
-    while (currentNode != NULL) {
-        pedge currentEdge = currentNode->first_edge;
-        while (currentEdge != NULL) {
-            pedge nextEdge = currentEdge->next;
-            free(currentEdge);
-            currentEdge = nextEdge;
-        }
-        pnode nextNode = currentNode->next;
-        free(currentNode);
-        currentNode = nextNode;
-    }
-    free(graph);
-}
-
